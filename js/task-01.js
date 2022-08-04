0341e39a34a9d3ce1c6ffe6c3e69b6e7252ca1f1
IlliaKlason <105161428+IlliaKlason@users.noreply.github.com>
2022-08-05 00:26:51 +0300

    add two task

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const categoriesList = document.querySelectorAll('.item');
+console.log(`Number of categories: ${categoriesList.length}`)
+
+categoriesList.forEach(item => {
+   let textElement = item.firstElementChild.textContent;
+   let amountElements = item.lastElementChild.children.length;
+   console.table("Category: " + textElement + "\n" + "Elements: " + amountElements);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listItems = document.querySelector("#ingredients");
+ingredients.map(ingredient => {
+  const liItem = document.createElement('li')
+  liItem.classList.add("item")
+  liItem.textContent = ingredient
+  listItems.append(liItem)
+})
+console.log(listItems);
+
+
