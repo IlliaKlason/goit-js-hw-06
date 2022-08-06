ac2ffd801f981edcf0cb4d5c4a8c4bc0d6c3b27a
IlliaKlason <105161428+IlliaKlason@users.noreply.github.com>
2022-08-06 17:54:44 +0300

    add 8-9 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,20 +1,22 @@
 const form = document.querySelector(".login-form");
-//  let inputEmail = document.querySelector("[type = 'email']");
-// let inputPassword = document.querySelector("[type = 'password']");
+const inputEmail = document.querySelector("[type = 'email']");
+const inputPassword = document.querySelector("[type = 'password']");
+
+
 form.addEventListener('submit', submit)
 
 function submit(elements) {
    elements.preventDefault()
-   const {
-      elements: { email, password },
-   } = elements.target
+   // const {
+   //    elements: { email, password },
+   // } = elements.target
 
-   if (email.value === '' || password.value === '') {
+   if (inputEmail.value === '' || inputPassword.value === '') {
       return alert('Привет Илья')
    }
    const userInfo = {
-      email: email.value,
-      password: password.value
+      email: inputEmail.value,
+      password: inputPassword.value
    }
 
    console.log(userInfo)
