1349b96a73c99b3a074ec329d5a939744f771b93
IlliaKlason <105161428+IlliaKlason@users.noreply.github.com>
2022-08-06 19:09:32 +0300

    Update task 10

--- js/task-10.js
+++ js/task-10.js
@@ -17,11 +17,14 @@ function createBoxes() {
   for (let i = 1; i <= number; i += 1) {
     const div = document.createElement("div");
     const p = document.createElement("p");
-    const color = getRandomHexColor()
-    p.textContent = "Илья";
+
+
     div.appendChild(p)
+    p.textContent = "Илья";
     p.style.fontSize = i * 2 + 1 + "px"
-    div.style.backgroundColor = color;
+    p.style.textAlign = "center"
+    p.style.verticalAlign = "middle"
+    div.style.backgroundColor = getRandomHexColor();
     div.style.width = i * 10 + 20 + "px";
     div.style.height = i * 10 + 20 + "px";
 
